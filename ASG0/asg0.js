9fc0533d9d0c6303d9085e6a109ff38656a6ee5c
Daniel John Carig <dc1029924@swccd.edu>
2022-01-03 10:54:52 -0800

    drew a red vector (v1) on the canvas; step 2 done

--- ASG0/asg0.js
+++ ASG0/asg0.js
@@ -1,11 +1,11 @@
 // Daniel John S. Carig
-// asg0
+// ASG0
 var ctx;
 var canvas;
 
 function main() {
   // Retrieve <canvas> element
-  canvas = document.getElementById('example');
+  canvas = document.getElementById('asg0');
   if (!canvas) {
     console.log('Failed to retrieve the <canvas> element');
     return false;
@@ -16,4 +16,16 @@ function main() {
   // Black background
   ctx.fillStyle = 'black'; // Set color to black
   ctx.fillRect(0, 0, 400, 400);        // Fill a rectangle with the color
+
+  // Draw new lines
+   var v1 = new Vector3([2.25, 2.25, 0.0]);
+   drawVector(v1,"red");
+}
+
+function drawVector(v, color){
+   ctx.strokeStyle = color; // Set color
+   ctx.beginPath();
+   ctx.moveTo(canvas.width/2, canvas.height/2);
+   ctx.lineTo(200+v.elements[0]*20, 200-v.elements[1]*20, v.elements[2]*20);
+   ctx.stroke();
 }
