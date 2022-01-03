76c931c17d6c07b12ac62ae9f1dfd806c7456f52
Daniel John Carig <dc1029924@swccd.edu>
2022-01-03 10:31:59 -0800

    Using black canvas instead of the blue rectangle

--- ASG0/asg0.js
+++ ASG0/asg0.js
@@ -1,16 +1,19 @@
-// DrawTriangle.js (c) 2012 matsuda
-function main() {  
+// Daniel John S. Carig
+// asg0
+var ctx;
+var canvas;
+
+function main() {
   // Retrieve <canvas> element
-  var canvas = document.getElementById('example');  
-  if (!canvas) { 
+  canvas = document.getElementById('example');
+  if (!canvas) {
     console.log('Failed to retrieve the <canvas> element');
-    return false; 
-  } 
-
+    return false;
+  }
   // Get the rendering context for 2DCG
-  var ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  // Black background
+  ctx.fillStyle = 'black'; // Set color to black
+  ctx.fillRect(0, 0, 400, 400);        // Fill a rectangle with the color
 }
