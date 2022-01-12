6eb2ace2b2caf5ab43fadedbcd9e138febc86384
Daniel John Carig <dc1029924@swccd.edu>
2022-01-12 07:46:14 -0800

    divided some parts of click() function into two separate functions

--- ASG1/asg1.js
+++ ASG1/asg1.js
@@ -67,7 +67,9 @@ function main() {
   connectVariablesToGLSL();
 
   // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position, u_FragColor) };
+  canvas.onmousedown = function(ev){
+    click(ev)
+  };
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -78,13 +80,10 @@ function main() {
 
 var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
-function click(ev, gl, canvas, a_Position, u_FragColor) {
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
-  var rect = ev.target.getBoundingClientRect();
 
-  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
-  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+function click(ev) {
+
+    var [x,y] = convertCoordinatesEventToGL(ev);
 
   // Store the coordinates to g_points array
   g_points.push([x, y]);
@@ -97,19 +96,41 @@ function click(ev, gl, canvas, a_Position, u_FragColor) {
     g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
   }
 
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  var len = g_points.length;
-  for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
-  }
+  // Draw every shape that is suppose to be in the canvas
+  renderAllShapes();
 }
+
+// Get Coordinates =================================================
+function convertCoordinatesEventToGL(ev){
+   var x = ev.clientX; // x coordinate of a mouse pointer
+   var y = ev.clientY; // y coordinate of a mouse pointer
+   var rect = ev.target.getBoundingClientRect() ;
+
+   // set coordinates based on origin
+   x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+   y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+
+   // Print coordinate in console
+   // console.log("("+x+","+y+")");
+
+   return [x,y];
+}
+
+// renderAllShapes =================================================
+function renderAllShapes(){
+   // Clear <canvas>
+   gl.clear(gl.COLOR_BUFFER_BIT);
+
+   var len = g_points.length;
+   for(var i = 0; i < len; i++) {
+     var xy = g_points[i];
+     var rgba = g_colors[i];
+
+     // Pass the position of a point to a_Position variable
+     gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+     // Pass the color of a point to u_FragColor variable
+     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+     // Draw
+     gl.drawArrays(gl.POINTS, 0, 1);
+   }
+ }
